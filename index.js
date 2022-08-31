86e1aa9a9084bafb20a543fe7b52e4d725af198a
Junior <vitteri81@gmail.com>
2022-08-31 13:54:09 -0300

    cambia nombres de variables

--- index.js
+++ index.js
@@ -1,56 +1,70 @@
 let jugadasMaquina = [];
-let jugadasJugador = [];
+let jugadasUsuario = [];
 
+const $jugar = document.querySelector("#jugar");
+const $contenedorBotones = document.querySelector("main");
 const $botones = document.querySelectorAll(".btn");
 
-document.querySelector("#jugar").onclick = manejarTurnoMaquina;
+agregarCursor($jugar);
+$jugar.onclick = manejarTurnoMaquina;
 
 function manejarTurnoMaquina() {
-  bloquearUsuario();
+  bloquearUsuario($contenedorBotones);
+  bloquearUsuario($jugar);
 
   $botonSecuenciaMaquina = obtenerBotonAleatorio();
   jugadasMaquina.push($botonSecuenciaMaquina);
 
-  const delayJugador = (jugadasMaquina.length + 1) * 1000;
+  const MILISEGUNDOS_DE_ESPERA_JUGADOR = (jugadasMaquina.length + 1) * 1000;
 
   jugadasMaquina.forEach(function ($boton, i) {
-    const delayMaquina = (i + 1) * 1200;
+    const MILISEGUNDOS_DE_ESPERA_MAQUINA = (i + 1) * 1200;
     setTimeout(function () {
       resaltar($boton);
-    }, delayMaquina);
+    }, MILISEGUNDOS_DE_ESPERA_MAQUINA);
   });
 
   setTimeout(function () {
-    desbloquearUsuario();
+    desbloquearUsuario($contenedorBotones);
     manejarTurnoJugador();
-  }, delayJugador);
+  }, MILISEGUNDOS_DE_ESPERA_JUGADOR);
 
-  jugadasJugador = [];
+  jugadasUsuario = [];
 }
 
-function pruebaUsuario(event) {
+function compararJugadas(event) {
   const $boton = event.target;
   resaltar($boton);
-  jugadasJugador.push($boton);
+  jugadasUsuario.push($boton);
 
-  const $botonMaquina = jugadasMaquina[jugadasJugador.length - 1];
+  const $botonMaquina = jugadasMaquina[jugadasUsuario.length - 1];
 
   if ($boton.id !== $botonMaquina.id) {
-    alert("perdiste");
+    alert("Perdiste, para volver a jugar presione el boton jugar.");
+    reiniciar();
     return;
   }
 
-  if (jugadasJugador.length === jugadasMaquina.length) {
-    bloquearUsuario();
-    setTimeout(manejarTurnoMaquina, 1000);
+  if (jugadasUsuario.length === jugadasMaquina.length) {
+    const MILISEGUNDOS_DE_ESPERA_COMPARACION = 1000;
+    bloquearUsuario($contenedorBotones);
+    setTimeout(manejarTurnoMaquina, MILISEGUNDOS_DE_ESPERA_COMPARACION);
   }
 }
 
-function resaltar(btn) {
-  btn.classList.add("resaltar");
+function reiniciar() {
+  jugadasUsuario = [];
+  jugadasMaquina = [];
+  bloquearUsuario($contenedorBotones);
+  desbloquearUsuario($jugar);
+}
+
+function resaltar($boton) {
+  $boton.classList.add("resaltar");
+  const MILISEGUNDOS_DE_ESPERA_BOTON_RESALTADO = 500;
   setTimeout(function () {
-    btn.classList.remove("resaltar");
-  }, 500);
+    $boton.classList.remove("resaltar");
+  }, MILISEGUNDOS_DE_ESPERA_BOTON_RESALTADO);
 }
 
 function obtenerBotonAleatorio() {
@@ -58,16 +72,25 @@ function obtenerBotonAleatorio() {
   return $botones[indiceAleatorio];
 }
 
-function bloquearUsuario() {
-  document.querySelector("main").classList.add("bloquear");
+function bloquearUsuario($elemento) {
+  $elemento.classList.add("bloquear");
 }
 
-function desbloquearUsuario() {
-  document.querySelector("main").classList.remove("bloquear");
+function desbloquearUsuario($elemento) {
+  $elemento.classList.remove("bloquear");
 }
 
 function manejarTurnoJugador() {
   $botones.forEach(function ($boton) {
-    $boton.onclick = pruebaUsuario;
+    agregarCursor($boton);
+    $boton.onclick = compararJugadas;
   });
+}
+
+function agregarCursor($elemento) {
+  $elemento.classList.add("cursor");
+}
+
+function eliminarCursor($elemento) {
+  $elemento.classList.remove("cursor");
 }
